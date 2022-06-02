8d9e1b281091da3e4284d073d1217853a4e5e7db
Kevin <kevin.511@outlook.com>
2022-06-02 13:50:09 -0500

    Implementando el tercer componente custom

--- react/components/AddtoCartInfo.tsx
+++ react/components/AddtoCartInfo.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
+import { useProduct } from 'vtex.product-context'
 
-const AddtoCartInfo = () => <div>Aqui va el tercer componente </div>
+const AddtoCartInfo = () => {
+  const productInfo = useProduct()
+
+  console.log('este producto tiene esta info', productInfo)
+
+  return <div>Aqui va el tercer componente </div>
+}
 
 export default AddtoCartInfo
